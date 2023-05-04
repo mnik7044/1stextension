b50acccec8dcf7113bd840430aba4f4cad43d5e9
Nikhil Mishra <108953684+mnik7044@users.noreply.github.com>
2023-05-04 06:17:34 +0530

    Added renderLeads function

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ let myLeads = []
 const inputEl = document.getElementById("input-el") 
 const inputBtn = document.getElementById("input-btn")
 
-const ulEl =document.getElementById("ul-el")
-inputBtn.addEvenetListener("click", function(){
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    renderLeads()
 }) 
 
+function renderLeads(){
+let listItems = ""
 for( let i =0; i<myLeads.length; i++)
 {
-    ulEl.textContent += myLeads[i] + " "
+    listItems += "<li>" + myLeads[i] + "</li>"
+}
+
+ulEl.innerHTML = listItems
 }
